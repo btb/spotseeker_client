7fa2c099a3f39060554554c2062780304d51e780
Bradley Bell <bradleyb@uw.edu>
2016-10-10 16:49:21 -0700

    implement json_data_structure

--- spotseeker_restclient/models/spot.py
+++ spotseeker_restclient/models/spot.py
@@ -47,6 +47,25 @@ class SpotImage(models.Model):
     upload_user = models.CharField(max_length=40)
     upload_application = models.CharField(max_length=100)
 
+    def json_data_structure(self):
+        return {
+            "id": self.pk,
+            "url": self.rest_url(),
+            "content-type": self.content_type,
+            "width": self.width,
+            "height": self.height,
+            "creation_date": self.creation_date.isoformat(),
+            "modification_date": self.modification_date.isoformat(),
+            "upload_user": self.upload_user,
+            "upload_application": self.upload_application,
+            # "thumbnail_root": reverse('spot-image-thumb',
+            #                           kwargs={'spot_id': self.spot.pk,
+            #                                   'image_id': self.pk}
+            #                           ).rstrip('/'),
+            "description": self.description,
+            "display_index": self.display_index
+        }
+
 
 class Spot(models.Model):
     """ Represents a place for students to study.
@@ -77,12 +96,97 @@ class Spot(models.Model):
                                    default=None,
                                    unique=True)
 
+    def json_data_structure(self):
+        """
+        Get a dictionary representing this spot which can be JSON encoded
+        """
+        extended_info = {}
+        info = self.extended_info
+        for attr in info:
+            extended_info[attr.key] = attr.value
+
+        available_hours = {
+            'monday': [],
+            'tuesday': [],
+            'wednesday': [],
+            'thursday': [],
+            'friday': [],
+            'saturday': [],
+            'sunday': [],
+        }
+
+        hours = sorted(self.spot_availability, key=lambda x: x.start_time)
+        for window in hours:
+            available_hours[window.get_day_display()].append(
+                window.json_data_structure())
+
+        images_set = sorted(self.images, key=lambda x: x.display_index)
+        images = [img.json_data_structure() for img in images_set]
+
+        types = [t.name for t in self.spot_types]
+
+        checkout_items = [item.json_data_structure() for item in
+                          self.items]
+
+        spot_json = {
+            "id": self.spot_id,
+            # "uri": self.rest_url(),
+            "etag": self.etag,
+            "name": self.name,
+            "type": types,
+            "location": {
+                # If any changes are made to this location dict,
+                # MAKE SURE to reflect those changes in the
+                # location_descriptors list in views/schema_gen.py
+                "latitude": self.latitude,
+                "longitude": self.longitude,
+                "height_from_sea_level": self.height_from_sea_level,
+                "building_name": self.building_name,
+                "floor": self.floor,
+                "room_number": self.room_number,
+            },
+            "capacity": self.capacity,
+            "display_access_restrictions":
+                self.display_access_restrictions,
+            "images": images,
+            "available_hours": available_hours,
+            "organization": self.organization,
+            "manager": self.manager,
+            "extended_info": extended_info,
+            "items": checkout_items,
+            "last_modified": self.last_modified.isoformat(),
+            "external_id": self.external_id
+        }
+        return spot_json
+
 
 class SpotItem(models.Model):
     item_id = models.IntegerField()
     name = models.CharField(max_length=100, blank=True)
     category = models.CharField(max_length=255)
     subcategory = models.CharField(max_length=255)
+
+    def json_data_structure(self):
+        extended = {}
+
+        for i in self.extended_info:
+            extended[i.key] = i.value
+
+        images = []
+
+        for image in self.images:
+            images.append(image.json_data_structure())
+
+        data = {
+            'id': self.item_id,
+            'name': self.name,
+            'category': self.category,
+            'subcategory': self.subcategory,
+            'extended_info': extended,
+            'images': images
+        }
+
+        return data
 
 
 class ItemImage(models.Model):
@@ -95,3 +199,21 @@ class ItemImage(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     upload_user = models.CharField(max_length=40)
     upload_application = models.CharField(max_length=100)
+
+    def json_data_structure(self):
+        return {
+            "id": self.image_id,
+            # "url": self.rest_url(),
+            "content-type": self.content_type,
+            "creation_date": self.creation_date.isoformat(),
+            "upload_user": self.upload_user,
+            "upload_application": self.upload_application,
+            # "thumbnail_root": reverse('item-image-thumb',
+            #                           kwargs={'item_id': self.item.pk,
+            #                                   'image_id': self.pk}
+            #                           ).rstrip('/'),
+            "description": self.description,
+            "display_index": self.display_index,
+            "width": self.width,
+            "height": self.height
+        }
